b419f8cd6948b6b7335198dac93af214088d49e7
tridung <tranductridung0103@gmail.com>
2025-01-11 11:28:18 +0700

    update run.sh

--- linux/test/run.sh
+++ linux/test/run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+CONTAINER_NAME="agent"
+
 PORTS=(
     "4444"
     "2222"
@@ -11,4 +14,6 @@ done
 
 sudo docker build -t agent .
 sudo apparmor_parser -r -W docker-network
-sudo docker run --cap-add=NET_ADMIN --net=host --privileged --rm -it $PORT_ARGS --security-opt apparmor=docker-network --name agent agent
+sudo docker run --cap-add=NET_ADMIN --net=host --privileged --rm -d -it $PORT_ARGS --security-opt apparmor=docker-network --name $CONTAINER_NAME $CONTAINER_NAME
+
+sudo docker exec $CONTAINER_NAME /bin/bash -c "cd /agent && ./run.sh"
